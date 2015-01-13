5e5ad1e14af3ff0aaab91dae76a79f853fcdb17f
Parker Zink <parker.zink@gmail.com>
2015-01-13 08:38:48 -0800

    Inital PHP function example files
    These are my three PHP public function example that I did all by myself.

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+function Name($name) {
+    echo "Hello world! My name is $name,";
+}
+Name("Parker");
+
+?>
+<?php
+function Age($age) {
+    echo " and I am $age years old!";
+}
+Age("16");
+
+?>
+<?php
+function Mascot($mascot) {
+    echo " I am also an official $mascot at Pasadena High School, which is of course, home of the bulldogs!<br>";
+}
+
+Mascot("bulldog");
+
